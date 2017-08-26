16d40a2828c100187372fd115c83fc11e175d9fb
tessy <tessyjosep1992@gmail.com>
2017-08-26 13:46:49 +0530

    Modified to fetch all the pages

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 import scrapy
 import json
+from urllib import urlencode
+
 
 class ClothingDescriptionSpdider(scrapy.Spider):
     name = "clothing"
@@ -9,7 +11,15 @@ class ClothingDescriptionSpdider(scrapy.Spider):
 
     def parse(self, response):
         jsonresponse = json.loads(response.body_as_unicode())
-        next_page = jsonresponse['product_pages']['next_page']
-        count_per_page = 20
+        has_next = jsonresponse['product_pages']['has_next']
+        url = 'https://api-cdn.chumbak.com/v1/category/474/products?'
         products = jsonresponse['products']
         print(json.dumps(products, indent=2))
+        if has_next != 0:
+            next_page = jsonresponse['product_pages']['next_page']
+            params = urlencode({"page": next_page, "count_per_page": 20})
+            url = url + params
+            yield scrapy.Request(
+                    url,
+                    callback=self.parse
+                )
